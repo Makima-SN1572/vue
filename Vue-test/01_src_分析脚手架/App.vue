5c4fc185a557a257a93601a81bfec10270e404bd
刘家豪 <2471654426@qq.com>
2022-10-26 16:10:59 +0800

    Add files via upload

---
+++ Vue-test/01_src_分析脚手架/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo">
+    <School />
+    <Student />
+  </div>
+</template>
+
+<script>
+//App是所有组件的汇总,所以我们要引入所有组件到这里
+//引入组件
+import School from "./components/School.vue";
+import Student from "./components/Student.vue";
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
+
+<style>
+</style>
